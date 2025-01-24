ceffed38050a975e26369a04ba34dc3912c27f69
Giuseppe Davide Salvatore Pomo <giuseppepomo8@gmail.com>
2025-01-24 16:58:04 +0100

    settimo commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,14 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer className="footer text-start text-secondary p-3">
+      <a href="#" className="text-white">
+        Italia
+      </a>{" "}
+      |{" "}
+      <a href="#" className="text-white">
+        English (UK)
+      </a>{" "}
+      <hr />
       <p>
         Copiright © 2024 <span className="text-white">Apple Inc.</span> Tutti i
         diritti riservati.
